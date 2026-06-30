60fa943e5dcdf9a412cf092c2a4485d07e2fdabd
Marcos Leandro <mleandrojr@yggdrasill.com.br>
2026-06-30 18:12:35 -0300

    Adicionadas proteções conta report de mensagens do bot e de admins

--- src/actions/report.rs
+++ src/actions/report.rs
@@ -10,7 +10,7 @@ pub async fn run(pool: MySqlPool, ctx: &Context) {
 
     let Some(chat) = &ctx.chat else { return };
     let Some(message) = &ctx.message else { return };
-    let Some(admin_mention) = message.mentions.iter().find(|mention| mention.username == "@admin") else {
+    let Some(_) = message.mentions.iter().find(|mention| mention.username == "@admin") else {
         return
     };
 
@@ -19,7 +19,21 @@ pub async fn run(pool: MySqlPool, ctx: &Context) {
 
 pub async fn report(pool: &MySqlPool, ctx: &Context, chat: &Chat, message: &Message) {
 
+    let target = message.reply_to_message.as_deref().unwrap_or(message);
+
+    if is_self_message(target) {
+        let text = ctx.t_db(pool, "selfReportMessage").await;
+        send_message(target, text.as_str()).await;
+        return;
+    }
+
     let admins: Vec<ChatAdministratorType> = chat.get_admins().await;
+    if is_admin_message(target, &admins) {
+        let text = ctx.t_db(pool, "adminReportMessage").await;
+        send_message(target, text.as_str()).await;
+        return;
+    }
+
     let text_reported = admins.iter().fold(
         ctx.t_db(pool, "reportMessage").await,
         |mut acc, admin| {
@@ -28,8 +42,22 @@ pub async fn report(pool: &MySqlPool, ctx: &Context, chat: &Chat, message: &Mess
         }
     );
 
-    let target = message.reply_to_message.as_deref().unwrap_or(message);
+
     send_message(target, text_reported.as_str()).await;
+}
+
+fn is_self_message(message: &Message) -> bool {
+    let bot_id: i64 = std::env::var("TELEGRAM_USER_ID")
+        .expect("TELEGRAM_USER_ID must be set")
+        .parse()
+        .expect("Failed to parse TELEGRAM_USER_ID");
+
+    message.data.from.as_ref().is_some_and(|from| from.id == bot_id)
+}
+
+fn is_admin_message(message: &Message, admins: &Vec<ChatAdministratorType>) -> bool {
+    let Some(from) = &message.data.from else { return false };
+    admins.iter().any(|admin| admin.user.id == from.id)
 }
 
 async fn send_message(message: &Message, text: &str) {
